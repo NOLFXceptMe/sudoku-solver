e7bd848369f8202b17a43448c9e1ef6f6b93ff1a
Naveen Kumar Molleti <nerd.naveen@gmail.com>
2019-09-07 22:55:20 -0700

    Simplify sdk solver

--- sdksolver.rb
+++ sdksolver.rb
@@ -1,17 +1,8 @@
 #!/usr/bin/env ruby
 
-DEBUG = false
-
 def log(s)
   puts "#{s}" if DEBUG
 end
-
-ROWS = ('A'..'I').map(&:to_s)
-COLS = (1..9).map(&:to_s)
-VALUES = (1..9).map(&:to_s).join
-ROW_NEIGHBORS = [ 'ABC', 'DEF', 'GHI']
-COL_NEIGHBORS = [ '123', '456', '789']
-GRID = ROWS.product(COLS)
 
 def solve(g)
   if(solved?(g))
@@ -27,72 +18,64 @@ def solve(g)
 
   # pick a value for the square
   g[r][c].chars.each do |v|
-    g2 = Marshal.load(Marshal.dump(g))
-    log "Trying #{r}#{c} : #{v}"
-
-    next if invalid?(g2, r, c, v)
-    next if remove_v(g2, r, c, v).nil?
-
-    log "Proceeding with value #{v} for #{r}#{c}"
-
-    solution = solve(g2)
-    next if solution.nil?
-
-    return solution 
+    solution = process(g, r, c, v)
+    return solution unless solution.nil?
   end
   
   return nil
 end
 
-def invalid?(grid, row, col, value)
-  grid[row][col] = value
+def process(grid, row, col, value)
+    g = Marshal.load(Marshal.dump(grid))
+    log "Trying #{row}#{col} : #{value}"
+
+    g[row][col] = value
+    return if invalid?(g, row, col)
+
+    return if UN[[row, col]]
+      .reject { |s| s.eql?([row, col]) }
+      .reject { |r, c| g[r][c].length == 1 }
+      .any? { |r, c|
+      g[r][c] = g[r][c].gsub(value, '')
+      invalid?(g, r, c)
+    }
+
+    return if RN[[row, col]]
+      .reject { |r, c| g[r][c].length == 1 }
+      .any? { |r, c|
+      g[r][c] = g[r][c].gsub(value, '')
+      invalid?(g, r, c)
+    }
+
+    return if CN[[row, col]]
+      .reject { |r, c| g[r][c].length == 1 }
+      .any? { |r, c|
+      g[r][c] = g[r][c].gsub(value, '')
+      invalid?(g, r, c)
+    }
+
+    log "Proceeding with value #{value} for #{row}#{col}"
+
+    solve(g)
+end
+
+def invalid?(grid, row, col)
+  value = grid[row][col]
 
   return false if value.length != 1
-  return (
-    row_neighbors(row, col).any? do |r, c|
-      grid[r][c].eql?(value)
-    end ||
 
-    col_neighbors(row, col).any? do |r, c|
-      grid[r][c].eql?(value)
-    end
+  return (
+    RN[[row, col]].any? { |r, c| grid[r][c].eql?(value) } ||
+    CN[[row, col]].any? { |r, c| grid[r][c].eql?(value) }
   )
 end
 
-def remove_v(grid, row, col, value)
-  unit_neighbors(row, col)
-    .reject { |s| s.eql?([row, col]) }
-    .reject { |r, c| grid[r][c].length == 1 }
-    .each { |r, c|
-    return if invalid?(grid, r, c, grid[r][c].gsub(value, ''))
-  }
-
-  row_neighbors(row, col)
-    .reject { |r, c| grid[r][c].length == 1 }
-    .each { |r, c|
-    return if invalid?(grid, r, c, grid[r][c].gsub(value, ''))
-  }
-  
-  col_neighbors(row, col)
-    .reject { |r, c| grid[r][c].length == 1 }
-    .each { |r, c|
-    return if invalid?(grid, r, c, grid[r][c].gsub(value, ''))
-  }
-
-  return true
-end
-
 def row_neighbors(r, c)
-  COLS.map { |col| [r, col] }.reject { |u| unit_neighbors(r, c).include?(u) }
+  COLS.map { |col| [r, col] }.reject { |u| UN[[r, c]].include?(u) }
 end
 
 def col_neighbors(r, c)
-  ROWS.map { |row| [row, c] }.reject { |u| unit_neighbors(r, c).include?(u) }
-end
-
-def unit_neighbors(r, c)
-  ROW_NEIGHBORS.find { |s| s.include?(r) }.chars
-    .product(COL_NEIGHBORS.find { |s| s.include?(c) }.chars)
+  ROWS.map { |row| [row, c] }.reject { |u| UN[[r, c]].include?(u) }
 end
 
 def solved?(g)
@@ -100,18 +83,17 @@ def solved?(g)
 end
 
 def prepare(grid)
-  ROW_NEIGHBORS.product(COL_NEIGHBORS).each { |urows, ucols|
-    values_present = urows.chars.product(ucols.chars)
-      .map { |r, c| grid[r][c] }
-      .reject { |v| v == '.' }
-      .join
+  UNITS.each { |urows, ucols|
+    cells = urows.chars.product(ucols.chars)
+    unit_values = cells.map { |r, c| grid[r][c] }.join.delete('.')
+    possibilites = VALUES.delete(unit_values)
 
-    urows.chars.product(ucols.chars).each { |r, c| 
-      grid[r][c] = VALUES.delete(values_present) if grid[r][c] == '.'
-    }
+    cells
+      .select {|r, c| grid[r][c] == '.' }
+      .each { |r, c| grid[r][c] = possibilites }
   }
 
-  return grid
+  grid
 end
 
 def write(grid)
@@ -119,7 +101,7 @@ def write(grid)
 end
 
 def parse(file)
-  return {} if file.nil?
+  raise "No input file provided" if file.nil?
 
   lines = File.read(file).split("\n")
   ROWS.zip(lines)
@@ -127,9 +109,28 @@ def parse(file)
     .map { |k, v| [k, COLS.zip(v.chars).to_h] }.to_h
 end
 
+ROWS = ('A'..'I').map(&:to_s)
+COLS = (1..9).map(&:to_s)
+GRID = ROWS.product(COLS)
+
+VALUES = (1..9).map(&:to_s).join
+
+ROW_NEIGHBORS = [ 'ABC', 'DEF', 'GHI']
+COL_NEIGHBORS = [ '123', '456', '789']
+UNITS = ROW_NEIGHBORS.product(COL_NEIGHBORS)
+
+UN = UNITS.map { |(r, c)|
+  cells = r.chars.product(c.chars)
+  cells.map { |cell| [cell, cells] }
+}.map(&:to_h).reduce(&:merge)
+
+RN = GRID.map { |(r, c)| [[r, c], row_neighbors(r, c)] }.to_h
+CN = GRID.map { |(r, c)| [[r, c], col_neighbors(r, c)] }.to_h
+
+DEBUG = false
+
 grid = parse(ARGV[0])
 puts "Initial state:"
 puts "#{write(grid)}"
 puts "Solution: "
 puts "#{write(solve(prepare(grid)))}"
-#solve(prepare(grid))
